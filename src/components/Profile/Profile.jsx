ebe1380f11d758c7c156e4d71d3cb7a317703296
amirgoody <amirhosynakhlaghian@gmail.com>
2025-07-24 13:02:19 +0330

    fixed profile slider

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,122 +5,128 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
+// import "swiper/css/pagination";
 
 // import required modules
 // import { Pagination } from 'swiper/modules';
 import "./Profile.css";
 
 export default function Profile() {
-  return (
-    <swiper-container
-      className="mySwiper"
-      autoplay={{
-        delay: 5000,
-        disableOnInteraction: false,
-      }}
-      loop={true}
-    >
-      <swiper-slide>
-        <img
-          className=" rounded-full border-4 border-amber-300"
-          src="./image/profile/testimonials-1.jpg"
-          alt=""
-        />
-        <h1 className="tracking-tighter text-lg font-sans-Demibold">
-          ساول گودمن
-        </h1>
-        <h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
-          مدیران عامل: پایه گذار
-        </h3>
-        <div>
-          <p className="text-sm font-sans-Medium tracking-tighter">
-            پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
-            درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
-            همیشه می خندد.
-          </p>
-        </div>
-      </swiper-slide>
-      <swiper-slide>
-        <img
-          className=" rounded-full border-4 border-amber-300"
-          src="./image/profile/testimonials-2.jpg"
-          alt=""
-        />
-        <h1 className="tracking-tighter text-lg font-sans-Demibold">
-          سارا ویلسون
-        </h1>
-        <h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
-          طراح
-        </h3>
-        <div>
-          <p className="text-sm font-sans-Medium tracking-tighter">
-            پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
-            درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
-            همیشه می خندد.
-          </p>
-        </div>
-      </swiper-slide>
-      <swiper-slide>
-        <img
-          className=" rounded-full border-4 border-amber-300"
-          src="./image/profile/testimonials-3.jpg"
-          alt=""
-        />
-        <h1 className="tracking-tighter text-lg font-sans-Demibold">
-          ینا کارلیس
-        </h1>
-        <h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
-          صاحب فروشگاه
-        </h3>
-        <div>
-          <p className="text-sm font-sans-Medium tracking-tighter">
-            پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
-            درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
-            همیشه می خندد.
-          </p>
-        </div>
-      </swiper-slide>
-      <swiper-slide>
-        <img
-          className=" rounded-full border-4 border-amber-300"
-          src="./image/profile/testimonials-4.jpg"
-          alt=""
-        />
-        <h1 className="tracking-tighter text-lg font-sans-Demibold">
-          مت براندون
-        </h1>
-        <h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
-          فریلنسر
-        </h3>
-        <div>
-          <p className="text-sm font-sans-Medium tracking-tighter">
-            پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
-            درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
-            همیشه می خندد.
-          </p>
-        </div>
-      </swiper-slide>
-      <swiper-slide>
-        <img
-          className=" rounded-full border-4 border-amber-300"
-          src="./image/profile/testimonials-5.jpg"
-          alt=""
-        />
-        <h1 className="tracking-tighter text-lg font-sans-Demibold">
-          جان لارسون
-        </h1>
-        <h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
-          کارآفرین
-        </h3>
-        <div>
-          <p className="text-sm font-sans-Medium tracking-tighter">
-            پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
-            درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
-            همیشه می خندد.
-          </p>
-        </div>
-      </swiper-slide>
-    </swiper-container>
-  );
+	return (
+		<Swiper
+			className="h-full *:py-8"
+			// spaceBetween={20}
+			slidesPerView={1}
+			// navigation={true}
+			autoplay={{
+				delay: 5000,
+				disableOnInteraction: false,
+			}}
+			pagination={{ clickable: true }}
+			loop={true}
+			modules={[Autoplay, Pagination, Navigation]}
+			// modules={[ Navigation]}
+		>
+			<SwiperSlide className="!flex flex-col items-center space-y-2">
+				<img
+					className=" rounded-full border-4 border-amber-300 size-25 object-cover"
+					src="./image/profile/testimonials-1.jpg"
+					alt=""
+				/>
+				<h1 className="tracking-tighter text-lg font-sans-Demibold">
+					ساول گودمن
+				</h1>
+				<h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
+					مدیران عامل: پایه گذار
+				</h3>
+				<div>
+					<p className="text-sm font-sans-Medium tracking-tighter">
+						پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
+						درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
+						همیشه می خندد.
+					</p>
+				</div>
+			</SwiperSlide>
+			<SwiperSlide className="!flex flex-col items-center space-y-2">
+				<img
+					className=" rounded-full border-4 border-amber-300 size-25 object-cover"
+					src="./image/profile/testimonials-2.jpg"
+					alt=""
+				/>
+				<h1 className="tracking-tighter text-lg font-sans-Demibold">
+					سارا ویلسون
+				</h1>
+				<h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
+					طراح
+				</h3>
+				<div>
+					<p className="text-sm font-sans-Medium tracking-tighter">
+						پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
+						درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
+						همیشه می خندد.
+					</p>
+				</div>
+			</SwiperSlide>
+			<SwiperSlide className="!flex flex-col items-center space-y-2">
+				<img
+					className=" rounded-full border-4 border-amber-300 size-25 object-cover"
+					src="./image/profile/testimonials-3.jpg"
+					alt=""
+				/>
+				<h1 className="tracking-tighter text-lg font-sans-Demibold">
+					ینا کارلیس
+				</h1>
+				<h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
+					صاحب فروشگاه
+				</h3>
+				<div>
+					<p className="text-sm font-sans-Medium tracking-tighter">
+						پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
+						درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
+						همیشه می خندد.
+					</p>
+				</div>
+			</SwiperSlide>
+			<SwiperSlide className="!flex flex-col items-center space-y-2">
+				<img
+					className=" rounded-full border-4 border-amber-300 size-25 object-cover"
+					src="./image/profile/testimonials-4.jpg"
+					alt=""
+				/>
+				<h1 className="tracking-tighter text-lg font-sans-Demibold">
+					مت براندون
+				</h1>
+				<h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
+					فریلنسر
+				</h3>
+				<div>
+					<p className="text-sm font-sans-Medium tracking-tighter">
+						پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
+						درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
+						همیشه می خندد.
+					</p>
+				</div>
+			</SwiperSlide>
+			<SwiperSlide className="!flex flex-col items-center space-y-2">
+				<img
+					className=" rounded-full border-4 border-amber-300 size-25 object-cover"
+					src="./image/profile/testimonials-5.jpg"
+					alt=""
+				/>
+				<h1 className="tracking-tighter text-lg font-sans-Demibold">
+					جان لارسون
+				</h1>
+				<h3 className="tracking-tighter text-sm text-zinc-200 font-sans-Medium">
+					کارآفرین
+				</h3>
+				<div>
+					<p className="text-sm font-sans-Medium tracking-tighter">
+						پیامدهای خالص شکر خالص تا زمانی که ورودی پرتقال را دریافت کند ، خوب
+						درمان می شوند. متهمان چه هستند ، فقط به نوعی بودجه نیاز دارد. میسن
+						همیشه می خندد.
+					</p>
+				</div>
+			</SwiperSlide>
+		</Swiper>
+	);
 }
